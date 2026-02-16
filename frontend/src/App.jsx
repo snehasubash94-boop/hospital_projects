8c98731d0e031fa1612b49d2337a3b3f41803a6c
Sneha Subash <snehasubash94@gmail.com>
2026-02-16 18:11:58 +0530

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,25 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
+import Myprofile from "./pages/Myprofile"
+import Navbar from "./components/Navbar"
 
-const App =() =>{
-  return(
-    <div  className='mx-4 sm:mx-[10%]'>
+
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
 
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointment' element={<MyAppointment />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
       </Routes>
-      
+
     </div>
   )
 }
