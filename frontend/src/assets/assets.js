50dd478104d84dc364ba310956ec24ef152e2ab9
Sneha Subash <snehasubash94@gmail.com>
2026-02-17 17:20:06 +0530

    assets

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,35 +1,23 @@
 import appointment_img from './appointment_img.png'
-import header_img from './header_img.png'
-import group_profiles from './group_profiles.png'
+import group_profiles from './group_profiles_png'
 import profile_pic from './profile_pic.png'
-import contact_image from './contact_image.png'
-import about_image from './about_image.png'
-import logo from './logo.svg'
-import dropdown_icon from './dropdown_icon.svg'
-import menu_icon from './menu_icon.svg'
-import cross_icon from './cross_icon.png'
-import chats_icon from './chats_icon.svg'
-import verified_icon from './verified_icon.svg'
-import arrow_icon from './arrow_icon.svg'
-import info_icon from './info_icon.svg'
-import upload_icon from './upload_icon.png'
+import contact_image from './contact_img.png'
+import about_image from './about_img.png'
 
 
-import stripe_logo from './stripe_logo.png'
-import razorpay_logo from './razorpay_logo.png'
-import KRISHNA from './KRISHNA.jpeg'
+
 import doc2 from './doc2.png'
 import doc3 from './doc3.png'
 import doc4 from './doc4.png'
 import doc5 from './doc5.png'
 import doc6 from './doc6.png'
-import doc7 from './doc7.png'
-import doc8 from './doc8.png'
-import doc9 from './doc9.png'
-import doc10 from './doc10.png'
-import doc11 from './doc11.png'
-import doc12 from './doc12.png'
-import doc13 from './doc13.png'
+// import doc7 from './doc7.png'
+// import doc8 from './doc8.png'
+// import doc9 from './doc9.png'
+// import doc10 from './doc10.png'
+// import doc11 from './doc11.png'
+// import doc12 from './doc12.png'
+// import doc13 from './doc13.png'
 
 
 import Dermatologist from './dermatologist.png';
@@ -37,26 +25,24 @@ import General_physician from './general_physician.png';
 import Gynecologist from './gynecologist.png';
 
 
-export const assets = {
-  appointment_img,
-  header_img,
-  group_profiles,
-  logo,
 
-  chats_icon,
-  verified_icon,
-  info_icon,
-  profile_pic,
-  arrow_icon,
-  contact_image,
-  about_image,
-  menu_icon,
-  cross_icon,
-  dropdown_icon,
-  upload_icon,
-  stripe_logo,
-  razorpay_logo,
-}
+
+
+//   verified_icon,
+//   info_icon,
+//   profchats_icon,
+//   ile_pic,
+//   arrow_icon,
+//   contact_image,
+//   about_image,
+//   menu_icon,
+//   cross_icon,
+//   dropdown_icon,
+//   upload_icon,
+//   stripe_logo,
+//   razorpay_logo,
+
+
 
 
 
@@ -84,6 +70,60 @@ export const specialitydata = [
 ];
 
   
+
+
+
+
+
+
+import appointment_img from "./appointment_img.png";
+import doc2 from "./doc2.png";
+export const assets = {
+  appointment_img,
+  doc2
+};
+
+
+
+import group_profiles from "./group_profiles.png";
+import doc3 from "./doc3.png";
+export const assets = {
+  group_profiles,
+  doc3
+};
+
+
+
+
+
+import profile_pic from "./profile_pic.png";
+import doc4 from "./doc4.png";
+export const assets = {
+  profile_pic,
+  doc4
+};
+
+
+
+
+// import contact_image from "./contact_img.png";
+// import doc5 from "./doc5.png";
+// export const assets = {
+//   contact_image,
+//   doc5
+// };
+
+
+
+
+
+// import about_image from "./about_img.png";
+// import doc6 from "./doc6.png";
+// export const assets = {
+//   about_image,
+//   doc6
+// };
+
 
 
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {assets} from '../assets/assets'
 import { NavLink } from 'react-router-dom'
-import { Navbar } from 'navbar'
+
 
 const Navbar = () => {
   return (
